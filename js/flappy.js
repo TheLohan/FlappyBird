048df26725f80e75b1908cab33af8945eedd0abb
Lohan Alexandre <Lohan_toniatti@hotmail.com>
2023-07-04 18:24:01 -0300

    Colisão

--- js/flappy.js
+++ js/flappy.js
@@ -104,6 +104,27 @@ function Progresso(){
     this.atualizarPontos(0);
 }
 
+function estaoSobrepostos(elementoA, elementoB){
+    const a = elementoA.getBoundingClientRect();
+    const b = elementoB.getBoundingClientRect();
+
+    const horizontal = a.left + a.width >= b.left && b.left + b.width >= a.left;
+    const vertical = a.top + a.height >= b.top && b.top + b.height >= a.top;
+
+    return horizontal && vertical;
+}
+
+function colidiu(passaro, barreiras){
+    let colidiu = false;
+    barreiras.pares.forEach(parDeBarreiras => {
+        if(!colidiu){
+            const superior = parDeBarreiras.superior.elemento;
+            const inferior = parDeBarreiras.inferior.elemento;
+            colidiu = estaoSobrepostos(passaro.elemento, superior) || estaoSobrepostos(passaro.elemento, inferior);
+        }
+    })
+    return colidiu;
+}
 function FlappyBird() {
     let pontos = 0;
     const areaDoJogo = document.querySelector('[wm-flappy');
@@ -111,7 +132,7 @@ function FlappyBird() {
     const largura = areaDoJogo.clientWidth;
 
     const progresso = new Progresso();
-    const barreiras = new Barreiras(700, 200, 200,400,()=>{
+    const barreiras = new Barreiras(700, 200, 1200,400,()=>{
         progresso.atualizarPontos(++pontos)
     });
     const passaro = new Passaro(altura);
@@ -124,6 +145,11 @@ function FlappyBird() {
         const temporizador = setInterval(() => {
             barreiras.animar()
             passaro.animar();
+
+            if(colidiu(passaro, barreiras)){
+                console.log("aaaa")
+                clearInterval(temporizador);
+            }
         },20)
     }
     
